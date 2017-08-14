1b57716a57ecce45f561367f653929d48c511875
Anjali-Rai-17 <anjali.ri042@gmail.com>
2017-08-14 20:25:45 +0530

    changed package name

--- AmazonPriceCheck/src/main/java/com/anjali/testamazon/Compare.java
+++ AmazonPriceCheck/src/main/java/com/anjali/testamazon/Compare.java
@@ -1,4 +1,4 @@
-package com.anjali;
+package com.anjali.testamazon;
 
 import java.util.Map;
 import java.util.Map.Entry;
--- AmazonPriceCheck/src/main/java/com/anjali/testamazon/ExcelUtlis.java
+++ AmazonPriceCheck/src/main/java/com/anjali/testamazon/ExcelUtlis.java
@@ -1,4 +1,4 @@
-package com.anjali;
+package com.anjali.testamazon;
 
 import java.io.File;
 import java.io.FileInputStream;
